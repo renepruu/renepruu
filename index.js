5cac5716b981a80957d6e8953c888166a8f32883
Rene Pruul <renepruu@users.noreply.github.com>
2024-10-03 15:32:08 +0300

    upload

--- index.js
+++ index.js
@@ -1,17 +1,22 @@
 const express = require("express");
 const dateTime = require("./")
+const fs = require("fs")
+//päringu lahtiarutamine(post)
+const bodyparser = require("body-parser");
+
 
 const app = express();
 app.set("view engine", "ejs")
 app.use(express.static("public"));
+//pilt = true, muu = false
+app.use(bodyparser.urlencoded({extended: false}));
 
 
 
 
-
-app.get("/", (req, res)=>{
+app.get("/regvisit", (req, res)=>{
     //res.send("Express läks tööle");
-    res.render("index.ejs");
+    res.render("regvisit");
 
 
 });
@@ -22,9 +27,42 @@ app.get("/timenow", (req, res)=>{
     const timeNow = dateTime.timeFormattedET();
 
     res.render("timenow", {nowWD: weekdayNow, nowD: dateNow, nowT: timeNow});
+});
+
+app.get("/vanasonad", (req, res) => {
+    	let folkWisdom = [];
+        fs.readFile("public/textfiles/vanasonad.txt", "utf8", (err, data)=>{
+            if(err){
+
+            
+            throw err;}
+            else {
+                folkWisdom = data.split(";");
+                res.render("justlist", {h2: "Vanasõnad", listData: folkWisdom});
+            }
+
+        });
+
+});
+
+app.post("regvisit", (reg, res)=>{
+    console.log(req.body);
+    fs.open("public/textfiles/visitlog.txt", "a", (err, file)=>{
+        if(err){
+            throw err;
+        }
+        else {
+            fs.appendFile("public/textfiles/visitlog.txt", req.body.firstNameInput + " " + req.body.lastNameInput + ";", (err=>{
+                if(err){
+                    throw err;
+                }
+                else {
+                    console.log("Salvestatud faili")
+                    res.render("regvisit");
+                }
+            }))
+        }
+    })
 })
-
-
-
 
 app.listen(5128);
---
+++ regvisit.js
@@ -0,0 +1,24 @@
+<html>
+<body>
+<hr>
+
+
+
+
+<li><a href="/">Avaleht</a></li>
+</hr>
+
+
+<h2>Pane oma nimi kirja</h2>
+	<form method="POST">
+	<label for="firstNameInput">Eesnimi: </label>
+	<input type="text" id="firstNameInput" name="firstNameInput" placeholder="Eesnimi ..">
+	<br></br>
+	<label for="lastNameInput">Perekonna nimi: </label>
+	<input type="text" id="lastNameInput" name="lastNameInput" placeholder="Perekonna nimi ..">
+	<input type="submit" id="regSubmit" name="regSubmit" value="Salvesta"></input>
+	</form>
+
+
+</body>
+</html>
